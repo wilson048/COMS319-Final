2ac745c5a9b3484cb8486144c21777279b5362a1
antzmon <79226059+antzmon@users.noreply.github.com>
2024-05-09 01:35:20 -0500

    changed a lot to make slot2 work

--- frontend/src/Slot2.js
+++ frontend/src/Slot2.js
@@ -1,217 +1,219 @@
-import React, { useState, useEffect } from "react";
+import React, { useStates, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import "bootstrap/dist/css/bootstrap.css";
 import accountDetails from "./SessionToken";
+import { Wheel } from "react-custom-roulette";
 import {
   BrowserRouter as Router,
   Route,
   Routes,
   useNavigate,
 } from "react-router-dom";
-import Slot4 from "./Slot4";
-
-function PullLever() {
-  let coins = accountDetails.coins;
-  if (coins < 20) {
-    alert(
-      "Brosky you are too poor for this. Go buy more they are literally free"
-    );
-  } else {
-    coins = coins - 20;
-    let reward = RunSlot2();
-    coins = coins + reward;
-    fetch(`http://localhost:8081/updateAccount/${accountDetails._id}`, {
-      method: "PUT",
-      headers: { "content-type": "application/json" },
-      body: JSON.stringify({
-        // _id: accountDetails._id, // also "id": req.body.id,
-        password: accountDetails.password, // also "name": req.body.name,
-        dob: accountDetails.dob, // also "price": req.body.price,
-        coins: coins, // all fresh accounts start with 500 coins
-        credit_card_num: accountDetails.credit_card_num, // credit card information is not initally saved
-        credit_card_name: accountDetails.credit_card_name,
-        credit_card_zip: accountDetails.credit_card_zip,
-        credit_card_cvv: accountDetails.credit_card_cvv,
-      }),
-    })
-      .then((response) => response.json())
-      .then((updateThisAccount) => {
-        accountDetails._id = updateThisAccount._id;
-        accountDetails.password = updateThisAccount.password;
-        accountDetails.dob = updateThisAccount.dob;
-        accountDetails.coins = updateThisAccount.coins;
-        accountDetails.credit_card_num = updateThisAccount.credit_card_num;
-        accountDetails.credit_card_name = updateThisAccount.credit_card_name;
-        accountDetails.credit_card_zip = updateThisAccount.credit_card_zip;
-        accountDetails.credit_card_cvv = updateThisAccount.credit_card_cvv;
-      });
-  }
-}
-/**
- *
- * @returns this function returns how much was won after spinning lot two
- */
-function RunSlot2() {
-  const slots = [0, 0];
-  let symbols = slot(slots);
-  let reward = Winner2(symbols);
-  return reward;
-}
-/**
- * used for different results to get a random number
- * @param {*} top the max for the random function
- * @returns the random result
- */
-function getRandom(top) {
-  let rand = Math.floor(Math.random() * top);
-  return rand;
-}
-/**
- * This makes the numbers for the slot machine
- * @param {*} slots the array of a size that is the number of slots in the machine
- * @returns an array of symbols that are the results
- */
-function slot(slots) {
-  for (let i = 0; i < slots.length; i++) {
-    slots[i] = getRandom(10);
-  }
-  let symbols = slotToChar(slot);
-  return symbols;
-}
-/**
- * cherry:h
- * lemon:l
- * orange:o
- * bell:e
- * grape:g
- * corn:c
- * bar:b
- * spade:s
- * watermelon:w
- * 7:7
- *
- * This takes the numbers from slots and turns in into letters
- * @param {*} slots this is the number array of the results
- */
-
-function slotToChar(slots) {
-  const symbols = [];
-  for (let i = 0; i < slots.length; i++) {
-    if (slots[i] == 0) {
-      symbols[i] = "h";
-    }
-    if (slots[i] == 1) {
-      symbols[i] = "l";
-    }
-    if (slots[i] == 2) {
-      symbols[i] = "o";
-    }
-    if (slots[i] == 3) {
-      symbols[i] = "e";
-    }
-    if (slots[i] == 4) {
-      symbols[i] = "g";
-    }
-    if (slots[i] == 5) {
-      symbols[i] = "c";
-    }
-    if (slots[i] == 6) {
-      symbols[i] = "b";
-    }
-    if (slots[i] == 7) {
-      symbols[i] = "s";
-    }
-    if (slots[i] == 8) {
-      symbols[i] = "w";
-    }
-    if (slots[i] == 9) {
-      symbols[i] = "7";
-    }
-  }
-  return symbols;
-}
-/**
- * This is for 2 row slots
- * @param {*} symbols the symbols that are the reults
- * @returns the amount earned
- */
-function Winner2(symbols) {
-  if (symbols[0] == "l" && symbols[1] == "l") return 0;
-  if (symbols[0] == "7" && symbols[1] == "7") return 50;
-  if (symbols[0] == "w" && symbols[1] == "w") return 35;
-  if (symbols[0] == "s" && symbols[1] == "s") return 25;
-  if (symbols[0] == "c" && symbols[1] == "c") return 15;
-  if (symbols[0] == symbols[1]) return 5;
-  if (symbols[1] == "h" || symbols[0] == "h") return 3;
-  if (symbols[1] == "g" || symbols[0] == "g") return 2;
-  return 0;
-}
+import "./Slots.css";
+const { useRef, useState } = React;
 
 function Slot2() {
+  const [fruit1, setFruit1] = useState("🍒");
+  const [fruit2, setFruit2] = useState("🍒");
+  const [rolling, setRolling] = useState(false);
+  let slotRef = [useRef(null), useRef(null)];
+  const fruits = ["🍒", "🍋", "🍊", "🔔", "🍇", "🌽", "💞", "♠️", "🍉", "7️⃣"];
+
+  // to trigger rolling and maintain state
+  const roll = () => {
+    setRolling(true);
+    setTimeout(() => {
+      setRolling(false);
+    }, 700);
+
+    // looping through all 3 slots to start rolling
+    const results = [];
+    slotRef.forEach((slot, i) => {
+      // this will trigger rolling effect
+      const selected = triggerSlotRotation(slot.current);
+      results[i] = selected;
+      if (i + 1 == 1) setFruit1(fruits[selected]);
+      else if (i + 1 == 2) setFruit2(fruits[selected]);
+    });
+    PullLever(results);
+  };
+
+  // this will create a rolling effect and return random selected option
+  const triggerSlotRotation = (ref) => {
+    function setTop(top) {
+      ref.style.top = `${top}px`;
+    }
+    let options = ref.children;
+    let randomOption = Math.floor(Math.random() * fruits.length);
+    let choosenOption = options[randomOption];
+    setTop(-choosenOption.offsetTop + 2);
+    //return fruits[randomOption];
+    return randomOption;
+  };
+
+  function PullLever(results) {
+    let coins = accountDetails.coins;
+    if (coins < 50) {
+      alert(
+        "Brosky you are too poor for this. Go more they are literally free"
+      );
+    } else {
+      coins = coins - 20;
+      let reward = RunSlot1(results);
+      coins = coins + reward;
+      fetch(`http://localhost:8081/updateAccount/${accountDetails._id}`, {
+        method: "PUT",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify({
+          // _id: accountDetails._id, // also "id": req.body.id,
+          password: accountDetails.password, // also "name": req.body.name,
+          dob: accountDetails.dob, // also "price": req.body.price,
+          coins: coins, // all fresh accounts start with 500 coins
+          credit_card_num: accountDetails.credit_card_num, // credit card information is not initally saved
+          credit_card_name: accountDetails.credit_card_name,
+          credit_card_zip: accountDetails.credit_card_zip,
+          credit_card_cvv: accountDetails.credit_card_cvv,
+        }),
+      })
+        .then((response) => response.json())
+        .then((updateThisAccount) => {
+          // accountDetails._id = updateThisAccount._id;
+          // accountDetails.password = updateThisAccount.password;
+          // accountDetails.dob = updateThisAccount.dob;
+          accountDetails.coins = coins;
+          // accountDetails.credit_card_num = updateThisAccount.credit_card_num;
+          // accountDetails.credit_card_name = updateThisAccount.credit_card_name;
+          // accountDetails.credit_card_zip = updateThisAccount.credit_card_zip;
+          // accountDetails.credit_card_cvv = updateThisAccount.credit_card_cvv;
+        });
+    }
+  }
+
+  /**
+   *
+   * @returns this function returns how much was won after spinning lot one
+   */
+  function RunSlot1(results) {
+    const slots = [0, 0];
+    let symbols = slotToChar(results);
+    let reward = Winner2(symbols);
+    return reward;
+  }
+  /**
+   * This makes the numbers for the slot machine
+   * @param {*} slots the array of a size that is the number of slots in the machine
+   * @returns an array of symbols that are the results
+   */
+  // function slot(slots) {
+  //   for (let i = 0; i < slots.length; i++) {
+  //     slots[i] = getRandom(10);
+  //   }
+  //   let symbols = slotToChar(slot);
+  //   return symbols;
+  // }
+  /**
+   * cherry:h /
+   * lemon:l/
+   * orange:o /
+   * bell:e /
+   * grape:g /
+   * corn:c/
+   * bar:b/
+   * spade:s/
+   * watermelon:w /
+   * 7:7/
+   *
+   * This takes the numbers from slots and turns in into letters
+   * @param {*} slots this is the number array of the results
+   */
+
+  function slotToChar(slots) {
+    const symbols = [];
+    for (let i = 0; i < slots.length; i++) {
+      if (slots[i] == 0) {
+        symbols[i] = "h";
+      }
+      if (slots[i] == 1) {
+        symbols[i] = "l";
+      }
+      if (slots[i] == 2) {
+        symbols[i] = "o";
+      }
+      if (slots[i] == 3) {
+        symbols[i] = "e";
+      }
+      if (slots[i] == 4) {
+        symbols[i] = "g";
+      }
+      if (slots[i] == 5) {
+        symbols[i] = "c";
+      }
+      if (slots[i] == 6) {
+        symbols[i] = "b";
+      }
+      if (slots[i] == 7) {
+        symbols[i] = "s";
+      }
+      if (slots[i] == 8) {
+        symbols[i] = "w";
+      }
+      if (slots[i] == 9) {
+        symbols[i] = "7";
+      }
+    }
+    return symbols;
+  }
+  /**
+   * This is for 2 row slots
+   * @param {*} symbols the symbols that are the reults
+   * @returns the amount earned
+   */
+  function Winner2(symbols) {
+    if (symbols[0] == "l" && symbols[1] == "l") return 0;
+    if (symbols[0] == "7" && symbols[1] == "7") return 50;
+    if (symbols[0] == "w" && symbols[1] == "w") return 35;
+    if (symbols[0] == "s" && symbols[1] == "s") return 25;
+    if (symbols[0] == "c" && symbols[1] == "c") return 15;
+    if (symbols[0] == symbols[1]) return 5;
+    if (symbols[1] == "h" || symbols[0] == "h") return 3;
+    if (symbols[1] == "g" || symbols[0] == "g") return 2;
+    return 0;
+  }
+
   return (
-    <div>
-      <div class="container">
-        <div class="col-md-7 col-lg-8">
-          <hr class="my-4"></hr>
-          <h4 class="mb-3">Log In</h4>
-
-          <div class="row g-3">
-            <div class="col-sm-6">
-              <label for="newUsername" class="form-label">
-                Username
-              </label>
-              <input
-                type="text"
-                class="form-control"
-                id="newUsername"
-                placeholder=""
-                required
-              ></input>
-              <div class="invalid-feedback">Valid ID is required</div>
-            </div>
-
-            <div class="col-sm-6">
-              <label for="newPassword" class="form-label">
-                Password
-              </label>
-              <input
-                type="password"
-                class="form-control"
-                id="newPassword"
-                placeholder=""
-                required
-              ></input>
-              <div class="invalid-feedback">Valid Title is required.</div>
-            </div>
+    <div className="SlotMachine">
+      <div className="slot">
+        <section>
+          <div className="container" ref={slotRef[0]}>
+            {fruits.map((fruit, i) => (
+              <div key={i}>
+                <span>{fruit}</span>
+              </div>
+            ))}
           </div>
-
-          {/* <hr class="my-4"></hr>
-            <div class="col-md-7 col-lg-8">
-              <button
-                class="w-50 btn btn-primary btn-lg"
-                onClick={() => addNewAccount()}
-              >
-                Create Account
-              </button>
-            </div> */}
-          <hr class="my-4 "></hr>
-          <div class="col-md-7 col-lg-8 d-flex gap-2 justify-content-center py-5"></div>
-        </div>
-        {/* <button
-            class="w-50 btn btn-primary btn-lg"
-            onClick={() => deleteAccount()}
-          >
-            Delete Account
-          </button>
-          <button
-            class="w-50 btn btn-primary btn-lg"
-            onClick={() => updateAccount()}
-          >
-            Update Coins
-          </button> */}
+        </section>
+      </div>
+      <div className="slot">
+        <section>
+          <div className="container" ref={slotRef[1]}>
+            {fruits.map((fruit) => (
+              <div>
+                <span>{fruit}</span>
+              </div>
+            ))}
+          </div>
+        </section>
+      </div>
+      <div
+        className={!rolling ? "roll rolling" : "roll"}
+        onClick={!rolling && roll}
+        disabled={rolling}
+      >
+        {rolling ? "Rolling..." : "Pull 50 coins"}
       </div>
     </div>
   );
 }
 
+//ReactDOM.render(<Slots />, document.getElementById("react-root"));
 export default Slot2;
---
+++ frontend/src/temp2.js
@@ -0,0 +1,217 @@
+import React, { useState, useEffect } from "react";
+import { useForm } from "react-hook-form";
+import "bootstrap/dist/css/bootstrap.css";
+import accountDetails from "./SessionToken";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useNavigate,
+} from "react-router-dom";
+import Slot4 from "./Slot4";
+
+function PullLever() {
+  let coins = accountDetails.coins;
+  if (coins < 20) {
+    alert(
+      "Brosky you are too poor for this. Go buy more they are literally free"
+    );
+  } else {
+    coins = coins - 20;
+    let reward = RunSlot2();
+    coins = coins + reward;
+    fetch(`http://localhost:8081/updateAccount/${accountDetails._id}`, {
+      method: "PUT",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify({
+        // _id: accountDetails._id, // also "id": req.body.id,
+        password: accountDetails.password, // also "name": req.body.name,
+        dob: accountDetails.dob, // also "price": req.body.price,
+        coins: coins, // all fresh accounts start with 500 coins
+        credit_card_num: accountDetails.credit_card_num, // credit card information is not initally saved
+        credit_card_name: accountDetails.credit_card_name,
+        credit_card_zip: accountDetails.credit_card_zip,
+        credit_card_cvv: accountDetails.credit_card_cvv,
+      }),
+    })
+      .then((response) => response.json())
+      .then((updateThisAccount) => {
+        accountDetails._id = updateThisAccount._id;
+        accountDetails.password = updateThisAccount.password;
+        accountDetails.dob = updateThisAccount.dob;
+        accountDetails.coins = updateThisAccount.coins;
+        accountDetails.credit_card_num = updateThisAccount.credit_card_num;
+        accountDetails.credit_card_name = updateThisAccount.credit_card_name;
+        accountDetails.credit_card_zip = updateThisAccount.credit_card_zip;
+        accountDetails.credit_card_cvv = updateThisAccount.credit_card_cvv;
+      });
+  }
+}
+/**
+ *
+ * @returns this function returns how much was won after spinning lot two
+ */
+function RunSlot2() {
+  const slots = [0, 0];
+  let symbols = slot(slots);
+  let reward = Winner2(symbols);
+  return reward;
+}
+/**
+ * used for different results to get a random number
+ * @param {*} top the max for the random function
+ * @returns the random result
+ */
+function getRandom(top) {
+  let rand = Math.floor(Math.random() * top);
+  return rand;
+}
+/**
+ * This makes the numbers for the slot machine
+ * @param {*} slots the array of a size that is the number of slots in the machine
+ * @returns an array of symbols that are the results
+ */
+function slot(slots) {
+  for (let i = 0; i < slots.length; i++) {
+    slots[i] = getRandom(10);
+  }
+  let symbols = slotToChar(slot);
+  return symbols;
+}
+/**
+ * cherry:h
+ * lemon:l
+ * orange:o
+ * bell:e
+ * grape:g
+ * corn:c
+ * bar:b
+ * spade:s
+ * watermelon:w
+ * 7:7
+ *
+ * This takes the numbers from slots and turns in into letters
+ * @param {*} slots this is the number array of the results
+ */
+
+function slotToChar(slots) {
+  const symbols = [];
+  for (let i = 0; i < slots.length; i++) {
+    if (slots[i] == 0) {
+      symbols[i] = "h";
+    }
+    if (slots[i] == 1) {
+      symbols[i] = "l";
+    }
+    if (slots[i] == 2) {
+      symbols[i] = "o";
+    }
+    if (slots[i] == 3) {
+      symbols[i] = "e";
+    }
+    if (slots[i] == 4) {
+      symbols[i] = "g";
+    }
+    if (slots[i] == 5) {
+      symbols[i] = "c";
+    }
+    if (slots[i] == 6) {
+      symbols[i] = "b";
+    }
+    if (slots[i] == 7) {
+      symbols[i] = "s";
+    }
+    if (slots[i] == 8) {
+      symbols[i] = "w";
+    }
+    if (slots[i] == 9) {
+      symbols[i] = "7";
+    }
+  }
+  return symbols;
+}
+/**
+ * This is for 2 row slots
+ * @param {*} symbols the symbols that are the reults
+ * @returns the amount earned
+ */
+function Winner2(symbols) {
+  if (symbols[0] == "l" && symbols[1] == "l") return 0;
+  if (symbols[0] == "7" && symbols[1] == "7") return 50;
+  if (symbols[0] == "w" && symbols[1] == "w") return 35;
+  if (symbols[0] == "s" && symbols[1] == "s") return 25;
+  if (symbols[0] == "c" && symbols[1] == "c") return 15;
+  if (symbols[0] == symbols[1]) return 5;
+  if (symbols[1] == "h" || symbols[0] == "h") return 3;
+  if (symbols[1] == "g" || symbols[0] == "g") return 2;
+  return 0;
+}
+
+function Slot2() {
+  return (
+    <div>
+      <div class="container">
+        <div class="col-md-7 col-lg-8">
+          <hr class="my-4"></hr>
+          <h4 class="mb-3">Log In</h4>
+
+          <div class="row g-3">
+            <div class="col-sm-6">
+              <label for="newUsername" class="form-label">
+                Username
+              </label>
+              <input
+                type="text"
+                class="form-control"
+                id="newUsername"
+                placeholder=""
+                required
+              ></input>
+              <div class="invalid-feedback">Valid ID is required</div>
+            </div>
+
+            <div class="col-sm-6">
+              <label for="newPassword" class="form-label">
+                Password
+              </label>
+              <input
+                type="password"
+                class="form-control"
+                id="newPassword"
+                placeholder=""
+                required
+              ></input>
+              <div class="invalid-feedback">Valid Title is required.</div>
+            </div>
+          </div>
+
+          {/* <hr class="my-4"></hr>
+            <div class="col-md-7 col-lg-8">
+              <button
+                class="w-50 btn btn-primary btn-lg"
+                onClick={() => addNewAccount()}
+              >
+                Create Account
+              </button>
+            </div> */}
+          <hr class="my-4 "></hr>
+          <div class="col-md-7 col-lg-8 d-flex gap-2 justify-content-center py-5"></div>
+        </div>
+        {/* <button
+            class="w-50 btn btn-primary btn-lg"
+            onClick={() => deleteAccount()}
+          >
+            Delete Account
+          </button>
+          <button
+            class="w-50 btn btn-primary btn-lg"
+            onClick={() => updateAccount()}
+          >
+            Update Coins
+          </button> */}
+      </div>
+    </div>
+  );
+}
+
+export default Slot2;
